193b0c48f463ea609b3dd0fea36d81a60b1d7e27
Anthony2511 <anthonybeaumecker@gmail.com>
2017-06-01 17:28:20 +0200

    regenerate brick

--- src/brick.js
+++ src/brick.js
@@ -13,6 +13,10 @@
      this.numberBrickLine = 9;
      this.arrayBrick = new Array( numberLine );
 
+     this.brickwidth = 45;
+     this.brickheight = 15;
+     this.spacebrick = 5;
+
 
    }
 
@@ -22,12 +26,31 @@
 
      aWin = 1;
      for ( i = 0; i < numberLine; i++ ) {
-       arrayBrick[i] = new Array( numberBrickLine );
-       ctx.fillStyle = color[i];
+       arrayBrick[ i ] = new Array( numberBrickLine );
+       ctx.fillStyle = color[ i ];
        for ( j=0; j < numberBrickLine; j++ ) {
          ctx.fillRect( ( j * ( width + space ) ), ( i * ( height + space ) ), width, height );
-         arrayBrick[i] [j] = 1;
+         arrayBrick[ i ] [ j ] = 1;
        }
+     }
+   }
+
+   regenerate() {
+     let { ctx, width, height, space } = this;
+     let color = [ "#e74c3c", "#c0392b", "#d35400", "#e67e22", "#1abc9c" ];
+
+     aWin = 1;
+     for ( i = 0; i < arrayBrick.length; i++ ) {
+       context.fillStyle = color[ i ];
+       for ( j = 0; j < arrayBrick [ i ].length; j++ ) {
+         if ( arrayBrick [ i ] [ j ] === 1 ) {
+           context.fillRect( ( j * ( brickwidth + spacebrick ) ), ( i * ( brickheight + spacebrick ) ), brickwidth, brickheight );
+           aWin = 0;
+         }
+       }
+     }
+     if ( aWin ) {
+       win();
      }
    }
  }
